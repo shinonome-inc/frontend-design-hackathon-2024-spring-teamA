aef4d5fe474dc1c20aa5a7f80508b4d2201e206c
Riku-Obata <riku.o-222-my.friend@ezweb.ne.jp>
2024-03-07 01:53:25 +0900

    選択肢の記述

--- src/pages/question1/index.jsx
+++ src/pages/question1/index.jsx
@@ -1,17 +1,27 @@
-import { paths } from '../../paths';
+import { paths } from "../../paths";
 import { useRouter } from "next/router";
+import React from "react";
+import styled from "styled-components";
 
 const Question1Page = () => {
   const router = useRouter();
   const onClick = () => {
     router.push(paths.question2);
-  }
+  };
   return (
     <div>
-      <h1>question1</h1>
-      <button onClick={onClick}>はい</button>
+      <h1>あなたは旅行で何をしたい？</h1>
+      <Button onClick={onClick}>観光地の散策</Button>
+      <Button onClick={onClick}>美味しいものを食べたい</Button>
+      <Button onClick={onClick}>アクティビティを楽しみたい</Button>
+      <Button onClick={onClick}>リフレッシュしたい</Button>
     </div>
   );
 };
 
 export default Question1Page;
+
+const Button = styled.button`
+  width: 50%;
+  background-color: green;
+`;
