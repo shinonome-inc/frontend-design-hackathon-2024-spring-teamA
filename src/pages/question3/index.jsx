7ce31bca0311dc7314ac21eb4996d3d877b8e7e3
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2024-03-12 22:46:09 +0900

    question3からqueryを用いたページ推移確認

--- src/pages/question3/index.jsx
+++ src/pages/question3/index.jsx
@@ -3,14 +3,19 @@ import { useRouter } from "next/router";
 
 const Question3Page = () => {
   const router = useRouter();
-  const onClick = () => {
-    const eachPath = paths.diagnosis + "/garden";
+  const onClick = ({query}) => {
+    const eachPath = paths.diagnosis + query;
     router.push(eachPath);
   }
   return (
     <div>
       <h1>question3</h1>
-      <button onClick={onClick}>{paths.recommend+"/garden"}</button>
+      <button onClick={onClick("/shrine1")}>{paths.recommend+"/shrine1"}</button>
+      <button onClick={onClick("/shrine2")}>{paths.recommend+"/shrine2"}</button>
+      <button onClick={onClick("/shrine3")}>{paths.recommend+"/shrine3"}</button>
+      <button onClick={onClick("/shrine4")}>{paths.recommend+"/shrine4"}</button>
+      <button onClick={onClick("/shrine5")}>{paths.recommend+"/shrine5"}</button>
+      <button onClick={onClick("/shrine6")}>{paths.recommend+"/shrine6"}</button>
     </div>
   );
 };
