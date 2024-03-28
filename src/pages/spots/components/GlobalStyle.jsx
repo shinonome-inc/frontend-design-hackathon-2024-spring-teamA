9b705729bec61d0dbf70f08b752fa848f02108f2
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2024-03-28 12:28:31 +0900

    bisyuusei

--- src/pages/spots/components/GlobalStyle.jsx
+++ src/pages/spots/components/GlobalStyle.jsx
@@ -1,12 +1,13 @@
 import styled, { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
-    @font-face {
-      font-family: "Kaisei HarunoUmi";
-      src: url("/fonts/KaiseiHarunoUmi-Bold.ttf") format("truetype");
-    }
-    @font-face {
-      font-family: "Noto Serif JP";
-      src: url("/fonts/NotoSerifJP-Medium.otf") format("opentype");
-    }
-  `;
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Kaisei HarunoUmi";
+    src: url("/fonts/KaiseiHarunoUmi-Bold.ttf") format("truetype");
+  }
+  @font-face {
+    font-family: "Noto Serif JP";
+    src: url("/fonts/NotoSerifJP-Medium.otf") format("opentype");
+  }
+`;
+export default GlobalStyle;
