0ffd591868b605bf4a730e66ce83315730bbcc64
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2024-03-28 13:03:44 +0900

    SSR実行

--- src/pages/spots/[locationId].jsx
+++ src/pages/spots/[locationId].jsx
@@ -62,6 +62,10 @@ const SpotsPage = () => {
 };
 export default SpotsPage;
 
+export async function getServerSideProps(context) {
+  return { props: data };
+}
+
 const AllWrapper = styled.div`
   box-sizing: border-box;
   width: 100%;
