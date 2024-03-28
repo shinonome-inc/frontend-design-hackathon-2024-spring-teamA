fe10a641392d05025b35e7fe01c0b4814bdf8cfb
JunPlayGround <j.masaki.23.5.946@4nonome.com>
2024-03-28 13:12:50 +0900

    sakujo

--- src/pages/spots/components/Spot.jsx
+++ src/pages/spots/components/Spot.jsx
@@ -26,10 +26,6 @@ const Spot = ({ spot }) => {
 };
 export default Spot;
 
-export async function getServerSideProps(context) {
-  return { props: spot };
-}
-
 const AllWrapper = styled.div`
   box-sizing: border-box;
   width: 100%;
